e6cd55d403240d93a1dddf24d6f763ef0a43bbe6
IgnacioCarbajoVallejo <ignacio.carbajo.vallejo@nfrance.com>
2017-04-26 15:58:11 +0200

    New funtions: Error and Date

--- main.go
+++ main.go
@@ -1,47 +1,56 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/mistifyio/go-zfs"
 )
 
-func main () {
-	// Get Clones
-	c, _ := zfs.GetDataset("tank/clones")
-	if c != nil {
-		// Destroy Clones
-		c.Destroy(zfs.DestroyRecursiveClones)
-	}
-
-	// Get Dataset
-	f, _ := zfs.GetDataset("tank/test")
-	if f != nil {
-		f.Destroy(zfs.DestroyRecursive)
-		zfs.CreateFilesystem("tank/test", nil)
-
-		// Create Snapshot
-		f.Snapshot("snap1", false)
-		s2, _ := f.Snapshot("snap2", false)
-		s3, _ := f.Snapshot("snap3", false)
-
-		// Create Clone
-		s2.Clone("tank/clones", nil)
-
-		// Rollback
-		s3.Rollback(true)
-	} else {
-		// Create Dataset
-		zfs.CreateFilesystem("tank/test", nil)
-
-		// Create Snapshot
-		f.Snapshot("snap1", false)
-		s2, _ := f.Snapshot("snap2", false)
-		s3, _ := f.Snapshot("snap3", false)
-
-		// Create Clone
-		s2.Clone("tank/clones", nil)
-
-		// Rollback
-		s3.Rollback(true)
+func ok(err error) {
+	if err != nil {
+		fmt.Printf("\033[31mUnexpected error! %s\033[39m\n", err.Error())
 	}
 }
 
+func snapName() string {
+	year, month, day := time.Now().Date()
+	hour, min, sec := time.Now().Clock()
+	snapDate := fmt.Sprintf("%s_%d-%s-%02d_%02d:%02d:%02d", "SNAP", year, month, day, hour, min, sec)
+	return snapDate
+}
+
+func main () {
+	// Get Clones
+	c, err := zfs.GetDataset("tank/clones")
+	ok(err)
+	if c != nil {
+		// Destroy Clones
+		c.Destroy(zfs.DestroyRecursiveClones)
+		ok(err)
+	}
+
+	// Get Dataset
+	f, err := zfs.GetDataset("tank/test")
+	ok(err)
+	if f != nil {
+		// Destroy Dataset
+		f.Destroy(zfs.DestroyRecursive)
+		ok(err)
+	}
+	// Create Dataset
+	zfs.CreateFilesystem("tank/test", nil)
+	ok(err)
+
+	// Create Snapshot
+	s, err := f.Snapshot(snapName(), false)
+	ok(err)
+
+	// Create Clone
+	s.Clone("tank/clones", nil)
+	ok(err)
+
+	// Rollback
+	s.Rollback(true)
+	ok(err)
+}
