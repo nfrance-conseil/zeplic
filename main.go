87e755ca5ee93e400a0abbe0f8d38d8321219c79
IgnacioCarbajoVallejo <ignacio.carbajo.vallejo@nfrance.com>
2017-04-28 14:29:28 +0200

    Deleting snapshot copies automatically

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/mistifyio/go-zfs"
@@ -13,19 +14,21 @@ import (
 type settings struct {
 	Dataset string
 	Clones  string
+	Retain  int
 }
 
-func config() (string, string) {
+func config() (string, string, int) {
 	configFile, err := ioutil.ReadFile("/etc/zeplic.d/config.json")
 	var jsontype settings
 	json.Unmarshal(configFile, &jsontype)
 	ok(err)
-	return jsontype.Dataset, jsontype.Clones
+	return jsontype.Dataset, jsontype.Clones, jsontype.Retain
 }
 
 func ok(err error) {
 	if err != nil {
 		fmt.Printf("\033[31mUnexpected error! %s\033[39m\n", err.Error())
+		// Call to Error() function in 'go-zfs' package
 		os.Stderr.WriteString(err.Error())
 	}
 }
@@ -34,46 +37,79 @@ func snapName() string {
 	year, month, day := time.Now().Date()
 	hour, min, sec := time.Now().Clock()
 	snapDate := fmt.Sprintf("%s_%d-%s-%02d_%02d:%02d:%02d", "SNAP", year, month, day, hour, min, sec)
+	// snapName == SNAP_yyyy-Month-dd_HH:MM:SS	
 	return snapDate
+}
+
+func between(value string, a string, b string) string {
+	posFirst := strings.Index(value, a)
+	if posFirst == -1 {
+		return ""
+	}
+	posLast := strings.Index(value, b)
+	if posLast == -1 {
+		return ""
+	}
+	posFirstAdjusted := posFirst + len(a)
+	if posFirstAdjusted >= posLast {
+		return ""
+	}
+	return value[posFirstAdjusted:posLast]
 }
 
 func main () {
 	//JSON
-	dataset, clones := config()
+	dataset, clones, retain := config()
 
-	// Get Clones
+	// Get clones dataset
 	cl, err := zfs.GetDataset(clones)
 	ok(err)
 	if cl != nil {
-		// Destroy Clones
+		// Destroy clones dataset
 		cl.Destroy(zfs.DestroyRecursiveClones)
 		ok(err)
 	}
 
-	// Get Dataset
+	// Get dataset (called in JSON file)
 	ds, err := zfs.GetDataset(dataset)
 	ok(err)
-	// Destroy Dataset
+	// Destroy dataset (optional)
 /*	ds.Destroy(zfs.DestroyRecursive)
 	ok(err)*/
 	if ds == nil {
-		// Create Dataset
+		// Create dataset if it does not exist
 		zfs.CreateFilesystem(dataset, nil)
 		ok(err)
 	}
 
-	// Get Snapshots
-/*	zfs.Snapshots(dataset)
-	ok(err)*/
-	// Create Snapshot
+	// Return the number of existing snapshots
+	count, err := zfs.Snapshots(dataset)
+	ok(err)
+	k := len(count)
+	if k > 0 {
+		// Save the last #Retain(JSON file) snapshots
+		for k > retain {
+			list, err := zfs.Snapshots(dataset)
+			ok(err)
+			justList := fmt.Sprintf("%s", list)
+			take := between(justList, "{", " ")
+			snap, err := zfs.GetDataset(take)
+			ok(err)
+			snap.Destroy(zfs.DestroyDefault)
+			ok(err)
+			k = k - 1
+		}
+	}
+
+	// Create new snapshot
 	s, err := ds.Snapshot(snapName(), false)
 	ok(err)
 
-	// Create Clone
+	// Create clone of last snapshot
 	s.Clone(clones, nil)
 	ok(err)
 
-	// Rollback
+	// Rollback snapshot
 /*	s.Rollback(true)
 	ok(err)*/
 }
